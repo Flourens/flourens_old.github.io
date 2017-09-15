2cde3ce99d8a3737a9e99f528a3f46b061b66924
Flourens <nvrevan@gmail.com>
2017-09-15 21:54:25 +0300

    RTU

--- test/assets/js/cloudapidemo/closure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-closure-compiler --js_output_file /tmp/cloud_api.js ./cloudapidemo.js raphael-min.js SimpleAjaxUploader.min.js
-cp ./cloudapidemo.css /tmp/cloud_api.css
-
-echo "Cloud JavaScript written to /tmp/cloud_api.js and /tmp/cloud_api.css"
