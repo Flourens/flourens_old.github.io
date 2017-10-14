572b3a9643a3cafbd762a804886a74d35ca09fac
Flourens <nvrevan@gmail.com>
2017-10-14 18:46:02 +0300

    1

--- drugdrop/js/common.js
+++ drugdrop/js/common.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
+	$(window).bind('dragover', dragover);
+		$(window).bind('drop', drop);
+		$('.overlay').bind('dragleave', dragleave);
+		var tid;
+
+		function dragover(event) {
+				clearTimeout(tid);
+				event.stopPropagation();
+				event.preventDefault();
+				$('.overlay').css('display', 'block');
+
+		}
+
+		function dragleave(event) {
+				tid = setTimeout(function(){
+				event.stopPropagation();
+				$('.overlay').css('display', 'none');
+				}, 100);
+		}
+
+		function drop(event) {
+				readfiles(event.originalEvent.dataTransfer.files);
+				event.stopPropagation();
+				event.preventDefault();
+				$('.overlay').css('display', 'none');
+		}
+	
+	
+	
   $(".navigation li a, .slide-button").on("click", function(e){
     e.preventDefault();
     var hrefval = $(this).attr("href");
-    
-    if(hrefval == ".about") {
+      if(hrefval == ".about") {
       var distance = $('.mainpage').css('right');
       
       if(distance == "auto" || distance == "0px") {
@@ -21,19 +49,29 @@ $(document).ready(function(){
   }); // end close button event handler
 	
 	$(".upload-btn").on("click", function(){
-		$('.overlay').addClass("overlay-on");
+		$('.overlay').css('display', 'block');
   });	
 	$(".overlay").on("click", function(){
-		$('.overlay').removeClass("overlay-on");
+		$('.overlay').css('display', 'none');
   }); 
+	
 	
   function openSidepage() {
 		$('#slide').addClass("slide");
 		$('.right').addClass("right-slide");
-    $('.mainpage').animate({
-      right: '985px'
-    }, 400, 'easeOutBack'); 
-  }
+		var winwidth = $(window).width() - 10;
+		if ($(window).width() > 1200) {
+			$('.aboutright').css('width', 985);
+			$('.mainpage').animate({
+				right: 968
+			}, 400, 'easeOutBack'); 
+		} else {
+			$('.aboutright').css('width', $(window).width());
+			$('.mainpage').animate({
+				right: $(window).width()
+			}, 400, 'easeOutBack'); 
+		}
+	}
   
   function closeSidepage(){
     $(".navigation li a").removeClass("open");
@@ -45,8 +83,8 @@ $(document).ready(function(){
   }
 	
 	
-	$('.mainpage, .aboutright').equalHeights();
-	window.addEventListener('resize', function () {
-		$('.mainpage, .aboutright').equalHeights();
-	})
+//	$('.mainpage, .aboutright').equalHeights();
+//	window.addEventListener('resize', function () {
+//		$('.mainpage, .aboutright').equalHeights();
+//	})
 });
