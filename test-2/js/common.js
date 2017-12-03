b4c03e93f2217412b502eee146790ef184cae1df
Flourens <nvrevan@gmail.com>
2017-12-03 04:09:16 +0200

    Test withot fixed header

---
+++ test-2/js/common.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+	$(function () {
+		$("#slider-range").slider({
+			range: true,
+			min: 1,
+			max: 999,
+			classes: {
+				"ui-slider": "new-slider",
+				"ui-slider-handle": "new-handle",
+  			"ui-slider-range": "new-range"
+			},
+			values: [1, 999],
+			slide: function (event, ui) {
+				$("#amount").val(ui.values[0] + "pt" + "      " + ui.values[1] + "pt");
+				$(".target-value-min-1").text(ui.values[0] + "pt");
+				$(".target-value-max-1").text(ui.values[1] + "pt");
+			}
+		});
+		$("#amount").val($("#slider-range").slider("values", 0) + "pt      " + $("#slider-range").slider("values", 1) + "pt");
+		$(".target-value-min-1").text($("#slider-range").slider("values", 0) + "pt");
+		$(".target-value-max-1").text($("#slider-range").slider("values", 1) + "pt");
+	});
+	
+	$(function () {
+		$("#slider-range-2").slider({
+			range: true,
+			min: 1,
+			max: 999,
+			classes: {
+				"ui-slider": "new-slider",
+				"ui-slider-handle": "new-handle",
+  			"ui-slider-range": "new-range"
+			},
+			values: [1, 999],
+			slide: function (event, ui) {
+				$("#amount-2").val(ui.values[0] + "stuk (s)" + "      " + ui.values[1] + "stuk (s)");
+				$(".target-value-min-2").text(ui.values[0] + "stuk (s)");
+				$(".target-value-max-2").text(ui.values[1] + "stuk (s)");
+			}
+		});
+		$("#amount-2").val($("#slider-range-2").slider("values", 0) + "stuk (s)      " + $("#slider-range").slider("values", 1) + "stuk (s)");
+		$(".target-value-min-2").text($("#slider-range-2").slider("values", 0) + "stuk (s)");
+		$(".target-value-max-2").text($("#slider-range-2").slider("values", 1) + "stuk (s)");
+	});
+	
+	$(function() {
+		$(".color-selector__item").click(function() {
+			$(".color-active").removeClass("color-active");
+			$(this).addClass("color-active");
+		});
+	});
+	
+});
