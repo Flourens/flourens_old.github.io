6ed1e775ca081fedade4a626588192e4de1a11b2
Flourens <nvrevan@gmail.com>
2017-12-04 18:27:21 +0200

    scroll

--- landing-test/js/common.js
+++ landing-test/js/common.js
@@ -11,11 +11,25 @@ $( document ).ready(function() {
 				headTop.removeClass('header-navbar__scrolled');
 				$(".header-navbar-image").attr("src", "img/invoicemap-logo-white.png")
 			}
-		
 		});
-		
 	});
 	
+	function scrollNav() {
+  $('.nav a, a.btn').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 60
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+	}
+scrollNav();
 });
 
 
