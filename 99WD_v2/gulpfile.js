4578dd2dd1dfc1d6a902b1e34db6e5d5de7e2688
Flourens <nvrevan@gmail.com>
2018-03-16 19:47:21 +0200

    v1 fix + v2

---
+++ 99WD_v2/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync').create();
+
+/*--- Errors ---*/
+var plumberErrorHandler = {
+	errorHandler: notify.onError({
+		title: 'Gulp',
+		message: 'Error: <%= error.message %>'
+	})
+};
+
+/*--- Browser-sync ---*/
+gulp.task('browser-sync', function () {
+	var files =[
+		'*/*.png',
+		'*/*.css',
+		'*.html',
+		'*/*.js'
+	];
+	browserSync.init(files, {
+		server: {
+			baseDir: ''
+		},
+		notify: false
+	});
+});
+
+//gulp.task('autoprefixer', () =>
+//    gulp.src('./app/css/main.css')
+//        .pipe(autoprefixer({
+//            browsers: ['last 2 versions'],
+//            cascade: false
+//        }))
+//        .pipe(gulp.dest('./app'))
+//);
+
+/*--- Gulp ---*/
+gulp.task('default', ['browser-sync']);
