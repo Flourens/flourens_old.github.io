9a2a2d43a8abd910f68b837f56640f968c6faa11
Flourens <nvrevan@gmail.com>
2018-04-17 22:21:06 +0300

    test_frontend_fin

---
+++ test_frontend_fin/inc/footer.php
@@ -0,0 +1,30 @@
+
+        <footer>
+        	<div class="row">
+        		<div class="column">
+		        	<p>
+		        		<strong>TEST</strong>
+						Rue du Test 201
+						3030 Milano
+		        	</p>
+		        </div>
+        		<div class="column">
+	        		<p><a href="" class="cta_btn">Demande d'offre</a></p>				
+							<p>
+								<span id="mail"></span>
+        			</p>
+	        	</div>
+	        </div>
+        </footer>
+
+     <script type="text/javascript" src="js/common.js"></script> 
+     
+		<!--  Email Obfuscator  -->
+     <script type="text/javascript">
+			<!--
+			var s="=b!isfg>#nbjmup;ifmmpAf.nbjm/dpn#!dmbtt>#dub`cuo#?Tfoe!bo!f.nbjm=0b?";
+			m=""; for (i=0; i<s.length; i++) m+=String.fromCharCode(s.charCodeAt(i)-1); document.getElementById('mail').innerHTML=(m);
+			//-->
+		</script>
+    </body>
+</html>
---
+++ test_frontend_fin/js/common.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', function(){
+(function() {  
+	let scrollpos = window.scrollY
+  const header = document.querySelector("#topbar")
+  const header_height = header.offsetHeight
+  const add_class_on_scroll = () => header.classList.add("scrolled")
+  const remove_class_on_scroll = () => header.classList.remove("scrolled")
+  
+	window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY;
+    if (scrollpos >= header_height) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+  });
+ })();
+
+
+//Smooth scroll animation
+
+	(function() {
+	var html, body;
+
+	window.onload = function() {
+		//getting all anchor elements
+		var links = document.links;
+
+		//getting html
+		html = document.documentElement;
+
+		//getting body
+		body = document.body;
+
+		for (var i = 0; i < links.length; i++) {
+			links[i].onclick = function() {
+				//getting current scroll position
+				var scrollTop = Math.round(body.scrollTop || html.scrollTop);
+				if (this.hash !== "") {
+					//preventing default anchor click behavior
+					event.preventDefault();
+
+					//getting element with id found in hash
+					var hashElement = document.getElementById(this.hash.substring(1));
+
+					var hashElementTop = 0;
+					var obj = hashElement;
+					while (obj.offsetParent) {
+						hashElementTop += obj.offsetTop;
+						obj = obj.offsetParent;
+					}
+					//getting element's position
+					hashElementTop = Math.round(hashElementTop);
+
+					scroll(scrollTop, hashElementTop, this.hash);
+				}
+			};
+		}
+	};
+
+	function scroll(from, to, hash) {
+		var timeInterval = 1; //in ms
+		var prevScrollTop;
+		var increment = to > from ? 10 : -10;
+
+		var scrollByPixel = setInterval(function() {
+			//getting current scroll position
+			var scrollTop = Math.round(body.scrollTop || html.scrollTop);
+
+			if (
+				prevScrollTop == scrollTop ||
+				(to > from && scrollTop >= to) ||
+				(to < from && scrollTop <= to)
+			) {
+				clearInterval(scrollByPixel);
+				//Add hash to the url after scrolling
+				window.location.hash = hash;
+			} else {
+				body.scrollTop += increment;
+				html.scrollTop += increment;
+
+				prevScrollTop = scrollTop;
+			}
+		}, timeInterval);
+	}
+
+	 })();
+	
+});
