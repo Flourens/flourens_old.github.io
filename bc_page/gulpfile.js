6167ffde926178799d2dfc7e67094d989605d117
Flourens <nvrevan@gmail.com>
2018-05-17 21:49:27 +0300

    bc_page

--- bc_page/gulpfile.js
+++ bc_page/gulpfile.js
@@ -37,6 +37,8 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/OwlCarousel/dist/owl.carousel.min.js',
+		'app/libs/popper/popper.js',
 		'app/libs/bootstrap/js/bootstrap.min.js',
 		'app/js/common.js', // Always at the end
 		])
